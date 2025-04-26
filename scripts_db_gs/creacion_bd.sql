36a931ff6d3c6f3832d55cb87e0a4383dedb9163
HerrSopMod <patchouli2025@gmail.com>
2025-04-26 18:41:44 -0500

    Prueba de scripts incompletos / faltan mejoras

---
+++ scripts_db_gs/creacion_bd.sql
@@ -0,0 +1,13 @@
+-- Eliminar la base de datos si ya existe
+DROP DATABASE IF EXISTS gestion_sistemas;
+
+-- Crear la base de datos con configuración regional adecuada
+CREATE DATABASE gestion_sistemas
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'es_PE.UTF-8'
+    LC_CTYPE = 'es_PE.UTF-8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = false;
