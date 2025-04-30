f438bb163570fae29f6922cb68c8f6d35c9c3c52
HerrSopMod <patchouli2025@gmail.com>
2025-04-30 02:11:40 -0500

    Detalles

--- scripts_db_gs/creacion_tablas.sql
+++ scripts_db_gs/creacion_tablas.sql
@@ -32,7 +32,7 @@ CREATE TABLE Comentario (
     fecha_comentario DATE NOT NULL,
     texto_comentario TEXT NOT NULL,
     id_origen INT NOT NULL,
-    tipo_origen VARCHAR(20) NOT NULL,
+    tipo_origen VARCHAR(50) NOT NULL,
     estado CHAR(1) NOT NULL,
     id_usuario INT NOT NULL,
     id_comentario_padre INT NOT NULL,
@@ -53,7 +53,7 @@ DROP TABLE IF EXISTS Estado_oc CASCADE;
 CREATE TABLE Estado_oc (
     cod_estado_oc INT NOT NULL,
     nombre_estado_oc CHAR(10) NOT NULL,
-    descripción_estado_oc VARCHAR(20) NOT NULL,
+    descripción_estado_oc VARCHAR(50) NOT NULL,
     PRIMARY KEY (cod_estado_oc)
 );
 
@@ -185,8 +185,8 @@ DROP TABLE IF EXISTS Opciones_comerciales CASCADE;
 CREATE TABLE Opciones_comerciales (
     id_opciones_comerciales INT NOT NULL,
     tipo_opcion CHAR(8) NOT NULL,
-    detalle_opcion VARCHAR(20) NOT NULL,
-    impacto_plazo VARCHAR(20) NOT NULL,
+    detalle_opcion VARCHAR(50) NOT NULL,
+    impacto_plazo VARCHAR(50) NOT NULL,
     nueva_estimacion_costo FLOAT NOT NULL,
     fecha_propuesta_o DATE NOT NULL,
     fecha_aprobacion_cliente_o DATE NOT NULL,
@@ -198,7 +198,7 @@ CREATE TABLE Personal_asignado (
     id_personal_asignado INT NOT NULL,
     nombre_personal CHAR(20) NOT NULL,
     telefono__personal INT NOT NULL,
-    disponibilidad_personal VARCHAR(20) NOT NULL,
+    disponibilidad_personal VARCHAR(50) NOT NULL,
     fecha_asignacion DATE NOT NULL,
     id_rol INT NOT NULL,
     PRIMARY KEY (id_personal_asignado),
@@ -220,7 +220,7 @@ CREATE TABLE Empresa (
     nombre_empresa CHAR(20) NOT NULL,
     tamaño_empresa INT NOT NULL,
     telefono_empresa INT NOT NULL,
-    web_corporativa VARCHAR(20) NOT NULL,
+    web_corporativa VARCHAR(50) NOT NULL,
     ranklng INT NOT NULL,
     cod_sector INT NOT NULL,
     PRIMARY KEY (id_empresa),
@@ -228,18 +228,28 @@ CREATE TABLE Empresa (
 );
 
 DROP TABLE IF EXISTS Solicitud_web CASCADE;
+
+-- Primero creamos el tipo ENUM con los 3 estados
+CREATE TYPE estado_solicitud_type AS ENUM (
+    'pendiente',
+    'revisada',
+    'contestada'
+);
+
+-- Luego creamos la tabla con el ENUM aplicado
 CREATE TABLE Solicitud_web (
     id_solicitud_web INT NOT NULL,
     nombre_solicitante CHAR(20) NOT NULL,
     apellido_solicitante CHAR(20) NOT NULL,
     telefono_solicitante INT NOT NULL,
-    correo_solicitante VARCHAR(20) NOT NULL,
+    correo_solicitante VARCHAR(50) NOT NULL,
     fecha_envio DATE NOT NULL,
-    estado_solicitud VARCHAR NOT NULL,
+    estado_solicitud estado_solicitud_type NOT NULL DEFAULT 'pendiente',
     id_empresa INT NOT NULL,
     PRIMARY KEY (id_solicitud_web),
     FOREIGN KEY (id_empresa) REFERENCES Empresa (id_empresa)
 );
+
 
 DROP TABLE IF EXISTS Asignación_recurso CASCADE;
 CREATE TABLE Asignación_recurso (
@@ -255,7 +265,7 @@ CREATE TABLE Asignación_recurso (
 DROP TABLE IF EXISTS Cliente CASCADE;
 CREATE TABLE Cliente (
     id_cliente INT NOT NULL,
-    direccion VARCHAR(20) NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
     nombre CHAR(10) NOT NULL,
     cant_contratos INT NOT NULL,
     num_ruc INT NOT NULL,
@@ -274,11 +284,11 @@ CREATE TABLE Cliente (
 DROP TABLE IF EXISTS Requisitos CASCADE;
 CREATE TABLE Requisitos (
     id_requisitos INT NOT NULL,
-    descripcion_r VARCHAR(20) NOT NULL,
+    descripcion_r VARCHAR(50) NOT NULL,
     prioridad_requisito INT NOT NULL,
     fecha_registro DATE NOT NULL,
     estado_r CHAR(8) NOT NULL,
-    historial_cambios VARCHAR(20) NOT NULL,
+    historial_cambios VARCHAR(50) NOT NULL,
     fecha_actualizacion DATE NOT NULL,
     id_cliente INT NOT NULL,
     PRIMARY KEY (id_requisitos),
@@ -332,7 +342,7 @@ CREATE TABLE Viabilidad_tecnica_requisitos (
 DROP TABLE IF EXISTS Evaluacion_financiera_requisitos CASCADE;
 CREATE TABLE Evaluacion_financiera_requisitos (
     id_evaluacion_f INT NOT NULL,
-    ajuste_presupuestal_necesario VARCHAR(20) NOT NULL,
+    ajuste_presupuestal_necesario VARCHAR(50) NOT NULL,
     costo_estimado_total FLOAT NOT NULL,
     fecha_evaluacion_f DATE NOT NULL,
     monto_ajustado FLOAT NOT NULL,
@@ -417,7 +427,7 @@ CREATE TABLE Propuesta (
 
 DROP TABLE IF EXISTS Solicitud CASCADE;
 CREATE TABLE Solicitud (
-    id_solicitud VARCHAR(20) NOT NULL,
+    id_solicitud VARCHAR(50) NOT NULL,
     descripción_solicitud VARCHAR(100) NOT NULL,
     fecha_solicitud DATE NOT NULL,
     fecha_firma DATE NOT NULL,
@@ -436,22 +446,33 @@ CREATE TABLE Contrato (
     id_contrato INT NOT NULL,
     fecha_contrato DATE NOT NULL,
     id_version INT NOT NULL,
-    id_solicitud VARCHAR(20) NOT NULL,
+    id_solicitud VARCHAR(50) NOT NULL,
     PRIMARY KEY (id_contrato),
     FOREIGN KEY (id_version) REFERENCES Version_contrato (id_version),
     FOREIGN KEY (id_solicitud) REFERENCES Solicitud (id_solicitud)
 );
 
 DROP TABLE IF EXISTS proyecto CASCADE;
+
+-- Creamos el tipo ENUM para los estados del proyecto
+CREATE TYPE estado_proyecto_type AS ENUM (
+    'Planificado',
+    'En Ejecución',
+    'Suspendido',
+    'Finalizado',
+    'Cancelado'
+);
+
+-- Creamos la tabla con el ENUM aplicado
 CREATE TABLE proyecto (
     cod_proyecto INT NOT NULL,
     nombre_proyecto CHAR(20) NOT NULL,
     descripción_proyecto VARCHAR(50) NOT NULL,
     presupuesto FLOAT NOT NULL,
-    estado_proyecto INT NOT NULL,
+    estado_proyecto estado_proyecto_type NOT NULL DEFAULT 'Planificado',
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE NOT NULL,
-    id_solicitud VARCHAR(20) NOT NULL,
+    id_solicitud VARCHAR(50) NOT NULL,
     cod_sector INT NOT NULL,
     PRIMARY KEY (cod_proyecto),
     FOREIGN KEY (id_solicitud) REFERENCES Solicitud (id_solicitud),
@@ -481,7 +502,7 @@ CREATE TABLE Gerente_Comercial (
     id_gerente_comercial INT NOT NULL,
     nombre_gerente CHAR(20) NOT NULL,
     telefono_gerente INT NOT NULL,
-    correo_gerente VARCHAR(20) NOT NULL,
+    correo_gerente VARCHAR(50) NOT NULL,
     id_usuario INT NOT NULL,
     id_validacion_presupuesto INT NOT NULL,
     PRIMARY KEY (id_gerente_comercial),
@@ -494,7 +515,7 @@ CREATE TABLE Ejecutivo_Comercial (
     id_ejecutivo_comercial INT NOT NULL,
     nombre_ejecutivo CHAR(15) NOT NULL,
     telefono_ejecutivo INT NOT NULL,
-    correo_ejecutivo VARCHAR(20) NOT NULL,
+    correo_ejecutivo VARCHAR(50) NOT NULL,
     id_usuario INT NOT NULL,
     id_termino INT NOT NULL,
     PRIMARY KEY (id_ejecutivo_comercial),
@@ -506,7 +527,7 @@ DROP TABLE IF EXISTS Contacto CASCADE;
 CREATE TABLE Contacto (
     id_contacto INT NOT NULL,
     nombre_contacto CHAR(15) NOT NULL,
-    email_contacto VARCHAR(20) NOT NULL,
+    email_contacto VARCHAR(50) NOT NULL,
     telefono_contacto INT NOT NULL,
     cargo_contacto CHAR(10) NOT NULL,
     origen CHAR(10) NOT NULL,
@@ -521,8 +542,8 @@ DROP TABLE IF EXISTS Visita CASCADE;
 CREATE TABLE Visita (
     id_visita INT NOT NULL,
     fecha_hora_visita DATE NOT NULL,
-    notas_visita VARCHAR(20) NOT NULL,
-    lugar_visita VARCHAR(20) NOT NULL,
+    notas_visita VARCHAR(50) NOT NULL,
+    lugar_visita VARCHAR(50) NOT NULL,
     estado_visita CHAR(10) NOT NULL,
     id_empresa INT NOT NULL,
     id_ejecutivo_comercial INT NOT NULL,
@@ -583,7 +604,7 @@ CREATE TABLE Viabilidad_tecnica (
 
 DROP TABLE IF EXISTS Equipo_técnico CASCADE;
 CREATE TABLE Equipo_técnico (
-    id_equipo VARCHAR(20) NOT NULL,
+    id_equipo VARCHAR(50) NOT NULL,
     nombre_equipo_tecnico CHAR(20) NOT NULL,
     capacidad INT NOT NULL,
     cod_especialidad INT NOT NULL,
@@ -600,7 +621,7 @@ CREATE TABLE Partner (
     id_partner INT NOT NULL,
     nombre_contacto CHAR(25) NOT NULL,
     telefono_contacto INT NOT NULL,
-    correo_contacto VARCHAR(20) NOT NULL,
+    correo_contacto VARCHAR(50) NOT NULL,
     especialidad_partner VARCHAR(15) NOT NULL,
     id_dolor INT NOT NULL,
     PRIMARY KEY (id_partner),
@@ -613,9 +634,9 @@ CREATE TABLE Decisor (
     nombre_decisor CHAR(15) NOT NULL,
     cargo_decisor CHAR(15) NOT NULL,
     telefono_decisor INT NOT NULL,
-    correo_decisor VARCHAR(20) NOT NULL,
+    correo_decisor VARCHAR(50) NOT NULL,
     RUC_decisor INT NOT NULL,
-    direccion_decisor VARCHAR(20) NOT NULL,
+    direccion_decisor VARCHAR(50) NOT NULL,
     id_dolor INT NOT NULL,
     PRIMARY KEY (id_decisor),
     FOREIGN KEY (id_dolor) REFERENCES Dolor (id_dolor)
@@ -627,10 +648,10 @@ CREATE TABLE Orden_Compra (
     fecha_oc DATE NOT NULL,
     precio_total FLOAT NOT NULL,
     fecha_entrega DATE NOT NULL,
-    id_solicitud VARCHAR(20) NOT NULL,
+    id_solicitud VARCHAR(50) NOT NULL,
     id_servicio INT NOT NULL,
     cod_estado_oc INT NOT NULL,
-    id_equipo VARCHAR(20) NOT NULL,
+    id_equipo VARCHAR(50) NOT NULL,
     PRIMARY KEY (id_oc),
     FOREIGN KEY (id_solicitud) REFERENCES Solicitud (id_solicitud),
     FOREIGN KEY (id_servicio) REFERENCES Cátalogo_servicios (id_servicio),
@@ -654,18 +675,26 @@ DROP TABLE IF EXISTS Dolor_equipo_tecnico CASCADE;
 CREATE TABLE Dolor_equipo_tecnico (
     fecha_asignacion DATE NOT NULL,
     id_dolor INT NOT NULL,
-    id_equipo VARCHAR(20) NOT NULL,
+    id_equipo VARCHAR(50) NOT NULL,
     PRIMARY KEY (id_dolor, id_equipo),
     FOREIGN KEY (id_dolor) REFERENCES Dolor (id_dolor),
     FOREIGN KEY (id_equipo) REFERENCES Equipo_técnico (id_equipo)
 );
 
 DROP TABLE IF EXISTS Encargo_comercial CASCADE;
+
+CREATE TYPE estado_encargo_type AS ENUM (
+    'pendiente',
+    'en progreso',
+    'completado'
+);
+
+-- Creamos la tabla con el ENUM aplicado (Encargo Comercial)
 CREATE TABLE Encargo_comercial (
     id_encargo INT NOT NULL,
     nombre_encargo CHAR(10) NOT NULL,
     fecha_asignacion DATE NOT NULL,
-    estado_encargo CHAR(10) NOT NULL,
+    estado_encargo estado_encargo_type NOT NULL DEFAULT 'pendiente',
     fecha_termino DATE NOT NULL,
     id_responsable INT NOT NULL,
     id_asignador INT NOT NULL,
@@ -673,6 +702,7 @@ CREATE TABLE Encargo_comercial (
     FOREIGN KEY (id_responsable) REFERENCES Ejecutivo_Comercial (id_ejecutivo_comercial),
     FOREIGN KEY (id_asignador) REFERENCES Gerente_Comercial (id_gerente_comercial)
 );
+
 
 DROP TABLE IF EXISTS Encargo_proyecto CASCADE;
 CREATE TABLE Encargo_proyecto (
@@ -686,7 +716,7 @@ DROP TABLE IF EXISTS Entregable_experiencia CASCADE;
 CREATE TABLE Entregable_experiencia (
     id_entregable INT NOT NULL,
     nombre_archivo CHAR(20) NOT NULL,
-    ruta_archivo VARCHAR(20) NOT NULL,
+    ruta_archivo VARCHAR(50) NOT NULL,
     fecha_entrega DATE NOT NULL,
     descripcion_entregable VARCHAR(50) NOT NULL,
     id_encargo INT NOT NULL,
